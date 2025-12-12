7f004730173132b26bc260d28734238bbe0543e4
pranalinandre29-glitch <pranalinandre29@gmail.com>
2025-12-13 01:09:44 +0530

    Update countnum.c

--- Assignment1/countnum.c
+++ Assignment1/countnum.c
@@ -1,22 +1,23 @@
 
 #include <stdio.h>
 
+int main() 
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
 int countOnes(int num) {
     int count = 0;
-    while (num != 0) {
-        if (num & 1) {   // check last bit
+    while (num != 0) 
+    {
+        if (num & 1)
+        {   // check last bit
             count++;
         }
         num = num >> 1;  // shift right
     }
     return count;
 }
-
-int main() {
-    int n;
-    printf("Enter number: ");
-    scanf("%d", &n);
-
     printf("Number of 1 bits = %d\n", countOnes(n));
     return 0;
 }
