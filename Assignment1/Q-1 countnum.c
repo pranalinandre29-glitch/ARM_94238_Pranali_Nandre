bd4280c6b2341626794c5529446ae1c2e677f19c
pranalinandre29-glitch <pranalinandre29@gmail.com>
2025-12-14 00:55:08 +0530

    Update Q-1 countnum.c

--- Assignment1/Q-1 countnum.c
+++ Assignment1/Q-1 countnum.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 
-<<<<<<< HEAD:Assignment1/Q-1 countnum.c
+
 int main() {
     int n;
     printf("Enter number: ");
@@ -13,16 +13,16 @@ int main()
     int n;
     printf("Enter number: ");
     scanf("%d", &n);
->>>>>>> 7f004730173132b26bc260d28734238bbe0543e4:Assignment1/countnum.c
+
 int countOnes(int num) {
     int count = 0;
     while (num != 0) 
     {
         if (num & 1)
-        {   // check last bit
+        {   
             count++;
         }
-        num = num >> 1;  // shift right
+        num = num >> 1;  
     }
     return count;
 }
